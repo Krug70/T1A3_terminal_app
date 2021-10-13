dd1d53a87cfaef8f28df10819aee99a385a0ed4b
leo <maurilio@inwind.it>
2021-10-13 15:53:50 +1000

    Added data gathering and coding

---
+++ T1A3.rb
@@ -0,0 +1,14 @@
+puts "Hi welcome to your Calories Counting App!"
+puts "Are you new here?"
+client = gets.chomp    
+if client == "yes" 
+    puts:"What do you want to be called? "
+    nickname = gets.chomp 
+    puts "Hi " + nickname + ", please give me the amount of calories you wanna stick to every day"
+    goal = gets.chomp.to_i
+    puts "Great! Remember to record here your calorie intake everytime you consume food"
+elsif client == "no"
+    puts "With what name did you record yourself?"
+else
+    puts "I don't understand your answer, can you please type yes or no?"
+end
