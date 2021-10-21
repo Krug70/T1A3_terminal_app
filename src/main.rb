eb1995f7a0cb9a6d2b37f4cb0b2e9efbaeb0f49f
leo <maurilio@inwind.it>
2021-10-21 15:37:43 +1000

    fixed problem with hashes reading

--- src/main.rb
+++ src/main.rb
@@ -70,11 +70,17 @@ puts "Hi " + name + " tell me your calorie goal for this meal"
 
 foods = {bread: 200, pasta: 250, rice: 180, apple: 50, banana: 80, egg: 75, chicken: 220, steak: 210, pork: 200, bacon: 400, beans: 110, tomato: 50, salad: 25, chips: 330, cheese: 450, sauce: 280, salami: 350, biscuit: 350, cola: 150, tuna: 180, milk: 150, juice: 110, hamburger: 550, muffin: 450}  
 
-def meal
-    "what are you having? (the calories are meant for 100 grams for each food)"
-    food = get.chomp 
-    if food.include? foods
-        intake += foods [food] 
+puts "what are you having? (the calories are meant for 100 grams for each food)"
+food = gets.chomp 
+
+# p food
+# # def meal (food)
+# p foods.key?(food)
+intake = 0
+   
+    if foods.key?(food.to_sym)
+        intake += foods [food.to_sym] 
+        puts "Ok I've added #{food} calories to your calories count of this meal"
     else
         puts "I don't have this food in my database, I'll need you to type in "
         puts "what's the food you are having?"
@@ -84,11 +90,12 @@ def meal
         foods.store(new_food, new_food_calories)
         intake += new_food_calories
     end
-end
+# end
 
-meal
 
-def calories_counting
+
+
+# def calories_counting
     if intake == calorie_goal
         puts "you have used all your calories for the meal"
     elsif intake >= calorie_goal
@@ -97,9 +104,9 @@ def calories_counting
         puts "you still have #{calorie_goal - intake} that you can use for this meal"
    
     end
-end
+# end
 
-calories_counting
+# calories_counting
 
 
 
