655b0ed20b016cfa164db8332fff7a38bb5e4e62
leo <maurilio@inwind.it>
2021-10-21 13:53:29 +1000

    fixed the syntax

--- src/main.rb
+++ src/main.rb
@@ -64,27 +64,30 @@
 
 
 puts "Hi what do you wanna be called? "
-    gets.chomp = name
-puts "Hi " + name + "tell me your calorie goal for this meal"
-    gets.chomp.to_i = calorie_goal
+    name = gets.chomp 
+puts "Hi " + name + " tell me your calorie goal for this meal"
+    calorie_goal = gets.chomp.to_i
+
+foods = {bread: 200, pasta: 250, rice: 180, apple: 50, banana: 80, egg: 75, chicken: 220, steak: 210, pork: 200, bacon: 400, beans: 110, tomato: 50, salad: 25, chips: 330, cheese: 450, sauce: 280, salami: 350, biscuit: 350, cola: 150, tuna: 180, milk: 150, juice: 110, hamburger: 550, muffin: 450}  
 
 def meal
     "what are you having? (the calories are meant for 100 grams for each food)"
-    get.chomp = food
+    food = get.chomp 
     if food.include? foods
         intake += foods [food] 
     else
         puts "I don't have this food in my database, I'll need you to type in "
         puts "what's the food you are having?"
-        gets.chomp = new_food
+        new_food = gets.chomp
         puts "please tell me what's the amount of calories for 100 grams of this food"
-        gets.chomp.to_i = new_food_calories
+        new_food_calories = gets.chomp.to_i 
         foods.store(new_food, new_food_calories)
         intake += new_food_calories
     end
 end
 
 meal
+
 def calories_counting
     if intake == calorie_goal
         puts "you have used all your calories for the meal"
@@ -92,14 +95,14 @@ def calories_counting
         puts "you went over your calorie gol of #{intake - calorie_goal} calories"
     else
         puts "you still have #{calorie_goal - intake} that you can use for this meal"
-    ensure
+   
     end
 end
 
+calories_counting
 
 
 
-foods {bread: 200, pasta: 250, rice: 180, apple: 50, banana: 80, egg: 75, chicken: 220, steak: 210, pork: 200, bacon: 400, beans: 110, tomato: 50, salad: 25, chips: 330, cheese: 450, sauce: 280, salami: 350, biscuit: 350, cola: 150, tuna: 180, milk: 150, juice: 110, hamburger: 550, muffin: 450}
 
 
 
