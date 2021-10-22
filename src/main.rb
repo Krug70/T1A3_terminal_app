4b698530967919f858bdf05118535c77b0ef0b38
leo <maurilio@inwind.it>
2021-10-22 13:29:36 +1000

    fine tutning, fixed the capital initials, spaces, got rid of the inactive code

--- src/main.rb
+++ src/main.rb
@@ -1,15 +1,15 @@
-puts "Hi what do you wanna be called? "
+puts "Hi! What do you wanna be called? "
     name = gets.chomp 
 puts "Hi " + name + " tell me your calorie goal for this meal"
     calorie_goal = gets.chomp.to_i
 
-foods = {bread: 200, pasta: 250, rice: 180, apple: 50, banana: 80, egg: 75, chicken: 220, steak: 210, pork: 200, bacon: 400, beans: 110, tomato: 50, salad: 25, chips: 330, cheese: 450, sauce: 280, salami: 350, biscuit: 350, cola: 150, tuna: 180, milk: 150, juice: 110, hamburger: 550, muffin: 450}  
+foods = {bread: 200, pasta: 250, rice: 180, apple: 50, banana: 80, egg: 75, chicken: 220, steak: 210, pork: 200, bacon: 400, beans: 110, tomato: 50, salad: 25, chips: 330, cheese: 450, sauce: 280, salami: 350, biscuit: 350, cola: 150, tuna: 180, milk: 150, juice: 110, hamburger: 550, muffin: 450, potatoes: 220}  
 
 intake = 0
 
 loop do
 
-puts "what are you having? (the calories are meant for 100 grams for each food)"
+puts "What are you having? (the calories are meant for 100 grams for each food)"
 food = gets.chomp 
 
    
@@ -17,8 +17,8 @@ food = gets.chomp
         intake += foods [food.to_sym] 
         puts "Ok I've added #{food} calories to your calories count of this meal"
     else
-        puts "I don't have this food in my database, I'll need you to type in "
-        puts "please tell me what's the amount of calories for 100 grams " + food
+        puts "I don't have this food in my database, I'll need you to type it in "
+        puts "Please tell me what's the amount of calories for 100 grams " + food
         new_food_calories = gets.chomp.to_i 
         new_hash = {food.to_sym => new_food_calories}
         foods.store(food.to_sym,new_food_calories)
@@ -26,26 +26,26 @@ food = gets.chomp
     end
 
     if intake == calorie_goal
-        puts "you have used all your calories for the meal"
+        puts "You have used all your calories for the meal"
     elsif intake >= calorie_goal
-        puts "you went over your calorie goal by #{intake - calorie_goal} calories"
+        puts "You went over your calorie goal by #{intake - calorie_goal} calories"
     else
-        puts "you still have #{calorie_goal - intake} calories that you can use for this meal"
+        puts "You still have #{calorie_goal - intake} calories that you can use for this meal"
     end
 
-    puts "do you want to add more food? (yes or no)"
+    puts "Do you want to add more food? (type yes or no)"
         response = gets.chomp  
     while response !="yes" and response !="no"
-        puts "please type yes or no"
+        puts "Please type yes or no"
         response = gets.chomp  
     end
     if response == "no"
         if intake == calorie_goal
-            puts "you have used all your calories for the meal"
+            puts "You have used all your calories for the meal"
         elsif intake >= calorie_goal
-            puts "you went over your calorie goal by #{intake - calorie_goal} calories"
+            puts "You went over your calorie goal by #{intake - calorie_goal} calories"
         else
-            puts "you saved #{calorie_goal - intake} calories for for this meal"
+            puts "You saved #{calorie_goal - intake} calories for for this meal"
         end
         break
     end
