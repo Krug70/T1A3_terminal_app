db8232df1f7a6c8315460dbd6dc98a1aa65bbd89
leo <maurilio@inwind.it>
2021-10-27 19:01:03 +1000

    error handling fixed

--- src/main.rb
+++ src/main.rb
@@ -5,16 +5,25 @@ x = gets
 puts "All you have to do is tell me your calorie goal for the meal, what are you having and I will workout how many calories you have got left!"
 puts "Please, tell me what do you wanna be called? "
     name = gets.chomp 
-puts "Hi " + name + " tell me your calorie goal for this meal"
-    calorie_goal = gets.chomp.to_i
 
-# # if 
-# #     calorie_goal.isNumbe
-    
-# else
-    
-# end
-    
+puts "Hi " + name + " tell me your calorie goal for this meal"
+    calorie_goal = gets.chomp
+
+def is_numeric(s)
+    begin
+        Integer(s)
+    rescue
+        puts "please enter a number" #in case the input is not numeric
+    end
+end
+
+while
+    !is_numeric(calorie_goal)
+    calorie_goal = gets.chomp
+end 
+
+calorie_goal = calorie_goal.to_i
+
 foods = {bread: 200, pasta: 250, rice: 180, apple: 50, banana: 80, egg: 75, chicken: 220, steak: 210, pork: 200, bacon: 400, beans: 110, tomato: 50, salad: 25, chips: 330, cheese: 450, sauce: 280, salami: 350, biscuit: 350, cola: 150, tuna: 180, milk: 150, juice: 110, hamburger: 550, muffin: 450, potatoes: 220}  
 
 intake = 0
@@ -33,12 +42,17 @@ loop do
     else
         puts "I don't have this food in my database, I'll need you to type it in "
         puts "Please tell me what's the amount of calories for 100 grams " + food
-        new_food_calories = gets.chomp.to_i 
+        new_food_calories = gets.chomp 
+        while
+            !is_numeric(new_food_calories)
+            new_food_calories = gets.chomp
+        end 
+        new_food_calories = new_food_calories.to_i
         new_hash = {food.to_sym => new_food_calories}
         foods.store(food.to_sym,new_food_calories)
         intake += new_food_calories
     end
-   
+
     if intake == calorie_goal
         puts "You have used all your calories for the meal"
     elsif intake >= calorie_goal
